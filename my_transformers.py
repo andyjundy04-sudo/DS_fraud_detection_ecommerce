340f5af930677aac074d14e80cdc7e60a457e456
andyjundy <andyjundy04@gmail.com>
2026-01-07 18:02:42 +0700

    update final trained dan tranformers

--- my_transformers.py
+++ my_transformers.py
@@ -20,3 +20,26 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         X['hour'] = tm.dt.hour
 
         return X.drop(columns=[self.col])
+
+#OOP convert category data (object to frequency)
+class FrequencyEncoder(BaseEstimator, TransformerMixin):
+  def __init__(self, cols):
+    self.cols = cols
+
+
+  def fit(self, X, y=None):
+    self.freq_maps_ = {}
+    X = X.copy()
+
+    for col in self.cols:
+      self.freq_maps_[col] = X[col].value_counts(normalize=True)
+
+    return self
+
+  def transform(self, X):
+    X = X.copy()
+
+    for col in self.cols:
+      X[col] = X[col].map(self.freq_maps_[col]).fillna(0)
+
+    return X
